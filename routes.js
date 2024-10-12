3926c27b848285a1d66ce3069178973dd4364073
Moises Lopes <moiseslopes999@gmail.com>
2024-10-12 14:26:10 -0300

    Atualização das Rotas

--- routes.js
+++ routes.js
@@ -7,19 +7,19 @@ const contatoController = require("./src/controllers/contatoController");
 const { erroServidor } = require("./src/middlewares/middleware");
 
 // ROTAS LOGIN
-route.get("/", loginController.index);
+route.get("/contato-facil", loginController.index);
 // route.get("/login/index", loginController.login)
-route.post("/login/register", loginController.register);
-route.post("/login/login", loginController.login);
-route.get("/login/logado", loginController.index);
-route.get("/login/logout", loginController.logout);
+route.post("/contato-facil/login/register", loginController.register);
+route.post("/contato-facil/login/login", loginController.login);
+route.get("/contato-facil/login/logado", loginController.index);
+route.get("/contato-facil/login/logout", loginController.logout);
 
 // ROTAS CONTATO
-route.get("/contato", contatoController.index);
-route.post("/contato/register", contatoController.register);
-route.get("/contato/index/:id", contatoController.editIndex);
-route.post("/contato/edit/:id", contatoController.edit);
-route.get("/contato/delete/:id", contatoController.delete)
+route.get("/contato-facil/contato", contatoController.index);
+route.post("/contato-facil/contato/register", contatoController.register);
+route.get("/contato-facil/contato/index/:id", contatoController.editIndex);
+route.post("/contato-facil/contato/edit/:id", contatoController.edit);
+route.get("/contato-facil/contato/delete/:id", contatoController.delete)
 // ROTA DE ERRO
 
 route.use((req, res , next) => {
